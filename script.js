bb4938f715921b8f4a67915799921fee90f0d82a
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-21 10:53:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+const buttonIncrease = document.querySelector(`.button-increase`);
+const buttonDecrease = document.querySelector(`.button-decrease`);
+const buttonReset = document.querySelector(`.button-reset`);
+const displayNumber = document.querySelector(`.display-number`);
+
+
